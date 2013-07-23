5123fc6c41b0a8d3586c0ff8fa58aa93e80c801a
Raoul Hecky <raoul.hecky@gmail.com>
2013-07-23 14:45:35 +0200

    Move username/pass/host to global config.js. Change home layout and use boostrap row-fluid/span4

--- js/controllers.js
+++ js/controllers.js
@@ -7,56 +7,72 @@ var home;
 function HomeCtrl($rootScope, $scope, $http) {
 
     var query = {
-	"cn_user": "alex",
-	"cn_pass": "qt4biasd",
+    "cn_user": calaosConfig.cn_user,
+	"cn_pass": calaosConfig.cn_pass,
 	"action": "get_home"
     };
 
-    $http.post('http://sailhas.enna.me/api.php', query).success(function(data) {
+    $http.post(calaosConfig.host, query).success(function(data) {
 	$scope.items = data;
 	$rootScope.home = data;
 	home = data;
 
+    $scope.items.home.sort(function (rooma, roomb) { return roomb.hits - rooma.hits; });
+
+    //create an array of max 3 rooms
+    $scope.homeByRow = [];
+    var a = [];
+    var cpt = 0;
+    for (var i = 0;i < $scope.items.home.length;i++) {
+        a.push(i);
+        cpt++;
+        if (cpt >= 3) {
+            $scope.homeByRow.push(a);
+            a = [];
+            cpt = 0;
+        }
+    }
+
     console.log(home);
 
     for(var i=0; i< data.home.length; i++) {
-	
-	switch(home.home[i].type) 
-	{ 
-	case "salon": 
+
+	switch(home.home[i].type)
+	{
+	case "salon":
 	    home.home[i].type = "lounge";
 	    break;
-	case "sam": 
+	case "sam":
 	    home.home[i].type = "dinningroom";
 	    break;
-	default: 
+	default:
 	    home.home[i].type = "various";
-	    break; 
+	    break;
 	}
 
     }
     });
 
 
-    $scope.orderProp = 'hint';
+//    $scope.orderProp = 'hint';
 }
 
 function RoomCtrl($rootScope, $scope, $routeParams, $http) {
 
     var query = {
-	"cn_user": "alex",
-	"cn_pass": "qt4biasd",
+	"cn_user": calaosConfig.cn_user,
+	"cn_pass": calaosConfig.cn_pass,
 	"action": "get_state",
 	"inputs": ["input_0"],
 	"outputs": ["output_0", "output_1"],
 	"audio_players": ["0"]
     };
-    
-    $http.post('http://sailhas.enna.me/api.php', query).success(function(data) {
-    	
+
+    $http.post(calaosConfig.host, query).success(function(data) {
+
 	for(var i=0; i< home.home.length; i++) {
     	    console.log(home.home[i].name);
-            
+
     	    if (home.home[i].name == $routeParams.room_name)
 	    {
     		$scope.room = home.home[i];
