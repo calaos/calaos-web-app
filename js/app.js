46b6ac3f6b0760744c2be500052b07571597b99f
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2013-09-06 00:28:32 +0200

    Add new directive contentItem to create different element depending on variable type

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@
 
 var calaos =  angular.module('calaos', ['ui.bootstrap']);
 
+
+
 calaos.config(['$routeProvider', function($routeProvider) {
     $routeProvider.
       when('/load', {templateUrl: 'partials/loading.html'}).
@@ -23,3 +25,76 @@ calaos.config(['$httpProvider', function($httpProvider) {
 function NavBarCtrl($scope) {
     $scope.isCollapsed = true;
 }
+
+calaos.directive('contentItem', function ($compile) {
+    var linker = function(scope, element, attrs) {
+    }
+    var intenalIntTemplate = '<h3>{{content.name}}<h3><div><input type="checkbox" checked></div>';
+
+    var getTemplate = function(contentType) {
+        var template = '';
+        switch(contentType) {
+	case 'InternalInt':
+            template = intenalIntTemplate;
+            break;
+/*
+       case 'light':
+            template = firstTemplate;
+            break;
+        case 'temp':
+            template = secondTemplate;
+            break;
+        case 'analog_in':
+            template = thirdTemplate;
+            break;
+	case 'analog_out':
+            template = thirdTemplate;
+            break;
+	case 'light_dimmer':
+            template = thirdTemplate;
+            break;
+	case 'light_rgb':
+            template = thirdTemplate;
+            break;
+	case 'shutter':
+            template = thirdTemplate;
+            break;
+	case 'shutter_mart':
+            template = thirdTemplate;
+            break;
+	case 'var_bool':
+	    template = thirdTemplate;
+	    break;
+	case 'var_int':
+            template = thirdTemplate;
+            break;
+	case 'var_string':
+	    template = thirdTemplate;
+	    break;
+	case 'scenario':
+	    template = thirdTemplate;
+	    break;
+*/
+	}
+	console.log("return " + template);
+	return template;
+    }
+
+    var linker = function(scope, element, attrs) {
+        scope.rootDirectory = 'images/';
+
+	console.log(scope.content.type);
+        element.html(getTemplate(scope.content.type));
+
+        $compile(element.contents())(scope);
+    }
+
+    return {
+        restrict: "E",
+        rep1ace: true,
+        link: linker,
+        scope: {
+            content:'='
+        }
+    };
+});
