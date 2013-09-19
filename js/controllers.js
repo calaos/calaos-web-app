38f6dc5eb2155cec7a6f93a2a4462dc2da178fc9
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2013-09-19 23:52:44 +0200

    Add new controller for color picker

--- js/controllers.js
+++ js/controllers.js
@@ -94,7 +94,6 @@ function RoomCtrl($rootScope, $scope, $routeParams, $http) {
 	}
     }
 
-
     $scope.getTemplateUrl = function(content) {
 	return content.gui_type ;
     }
@@ -132,5 +131,14 @@ function SettingsCtrl($scope) {
 	setCookie('cn_pass', $scope.cn_pass, 0);
 	setCookie('use_calaosnetwork', $scope.use_calaosnetwork, 365);
 	setCookie('host', $scope.host, 365);
+    }
+}
+
+function ColorPickerCtrl($scope) {
+
+    console.log($scope.$parent);
+
+    $scope.selectColor = function() {
+	console.log("colorPicker click");
     }
 }
