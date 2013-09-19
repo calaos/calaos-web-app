33f2ed49426375aa8fa200a55e1c5ac1e5d03031
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2013-09-19 23:54:32 +0200

    Add route for color picker

--- js/app.js
+++ js/app.js
@@ -2,9 +2,7 @@
 
 /* App Module */
 
-var calaos =  angular.module('calaos', ['ngRoute', 'ngAnimate', 'ui.bootstrap']);
-
-
+var calaos =  angular.module('calaos', ['ngRoute', 'ngAnimate']);
 
 calaos.config(['$routeProvider', function($routeProvider) {
     $routeProvider.
@@ -12,10 +10,11 @@ calaos.config(['$routeProvider', function($routeProvider) {
       when('/mobile/home',  {templateUrl: 'partials/mobile/home.html',   controller: 'RoomsListCtrl'}).
       when('/mobile/room/:room_name', {templateUrl: 'partials/mobile/room.html',  controller: 'RoomCtrl'}).
       when('/mobile/settings', {templateUrl: 'partials/mobile/settings.html',  controller: 'SettingsCtrl'}).
+      when('/mobile/color-picker', {templateUrl: 'partials/mobile/color-picker.html',  controller: 'ColorPickerCtrl'}).
       when('/desktop/home',  {templateUrl: 'partials/desktop/home.html',   controller: 'RoomsListCtrl'}).
       when('/desktop/room/:room_name', {templateUrl: 'partials/desktop/room.html',  controller: 'RoomCtrl'}).
       when('/desktop/settings', {templateUrl: 'partials/desktop/settings.html',  controller: 'SettingsCtrl'}).
-	  otherwise({redirectTo: '/load'})}]);
+	  otherwise({rebdirectTo: '/load'})}]);
 
 calaos.config(['$httpProvider', function($httpProvider) {
     $httpProvider.defaults.useXDomain = true;
