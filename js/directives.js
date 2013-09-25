55355892fb7c4babf7ca4f79f0771d3fd9e0e036
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2013-09-25 08:23:59 +0200

    resize color-picker to fit window size

--- js/directives.js
+++ js/directives.js
@@ -25,7 +25,7 @@ calaos.directive("colorPicker", function(){
             image.src = scope.url;
 
             image.onload = function () {
-                //ctx.scale( window.innerWidth / image.width , window.innerWidth / image.height);
+                ctx.scale( window.innerWidth / image.width , window.innerWidth / image.height);
                 ctx.drawImage(image, 0, 0, image.width, image.height); // draw the image on the canvas
             }
 
