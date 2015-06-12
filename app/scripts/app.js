11cdda44e253cb4805b4ad099abf0dc2f86079df
Raoul Hecky <raoul.hecky@gmail.com>
2015-06-12 15:39:01 +0200

    Add angular ui-route

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,21 +7,32 @@ angular
         'ngCookies',
         'ngMessages',
         'ngResource',
-        'ngRoute',
         'ngSanitize',
         'ngTouch',
         'mgcrea.ngStrap',
+        'ui.router',
     ])
-    .config(function ($stateProvider) {
+    .config(function ($stateProvider, $urlRouterProvider) {
+
+        //For any unmatched url, redirect to /home
+        $urlRouterProvider.otherwise('/home');
+
+        //Set up the states
         $stateProvider
             .state('login', {
-                url: '/login',
+                templateUrl: 'views/login.html',
                 data: {
                     requireLogin: false
                 }
             })
             .state('home', {
-                abstract: true,
+                templateUrl: 'views/home.html',
+                data: {
+                    requireLogin: true
+                }
+            })
+            .state('home.rooms', {
+                templateUrl: 'views/home.html',
                 data: {
                     requireLogin: true
                 }
