7372422c42d7e2fd5630f82e23f4fc235559597f
Raoul Hecky <raoul.hecky@gmail.com>
2015-06-14 18:00:43 +0200

    Add reminder

--- app/scripts/dev_config.js
+++ app/scripts/dev_config.js
@@ -3,6 +3,9 @@
  * Here you can override the default detected ws://hostname/api/v3 used by
  * the app. It allows you to run grunt server (on 9000 port) and connect to
  * a your calaos_server on a different host:port.
+ *
+ * Note: don't forget: git update-index --assume-unchanged app/scripts/dev_config.js
+ * to avoid commit this file with non default values!
  */
 
  var calaosDevConfig = {
