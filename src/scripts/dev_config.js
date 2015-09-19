f79f4f018ecce61c003c1235e0bce2aaab8b2729
Raoul Hecky <raoul.hecky@gmail.com>
2015-09-19 09:46:52 +0200

    update dev config

--- src/scripts/dev_config.js
+++ src/scripts/dev_config.js
@@ -1,10 +1,10 @@
 
 /*
- * Here you can override the default detected ws://hostname/api/v3 used by
- * the app. It allows you to run grunt server (on 9000 port) and connect to
+ * Here you can override the default detected ws://hostname/api used by
+ * the app. It allows you to run gulp serve (on 9000 port) and connect to
  * a your calaos_server on a different host:port.
  *
- * Note: don't forget: git update-index --assume-unchanged app/scripts/dev_config.js
+ * Note: don't forget: git update-index --assume-unchanged src/scripts/dev_config.js
  * to avoid commit this file with non default values!
  */
 
