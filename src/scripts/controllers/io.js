0fe76d66ea77fd695a37ce4b9b45e710552ff705
Raoul Hecky <r.hecky@emc-partner.ch>
2015-09-22 14:09:21 +0200

    implement controller for dimmer

--- src/scripts/controllers/io.js
+++ src/scripts/controllers/io.js
@@ -3,6 +3,32 @@
 angular.module('calaosApp')
 .controller('LightDimmerCtrl', ['$scope', '$state', 'CalaosApp', function ($scope, $state, CalaosApp) {
 
+    var updateState = function (item) {
+        $scope.percent_value = 0.0;
+        $scope.bool_status = false;
+
+        if (!isNaN(parseInt(item.state)))
+            $scope.percent_value = parseInt(item.state);
+        else if (item.state.substr(0, 4) == "set ")
+            $scope.percent_value = parseInt(item.state.substr(4, item.state.length - 4));
+        else if (item.state == "true")
+            $scope.percent_value = 100;
+        else if (item.state == "false")
+            $scope.percent_value = 0;
+
+        $scope.bool_status = $scope.percent_value > 0?true:false;
+        $scope.percent_value_rw = $scope.percent_value;
+    }
+
+    $scope.changeValueDimmer = function (item) {
+        var s = "set " + $scope.percent_value_rw;
+        CalaosApp.setState(item, s);
+    }
+
+    updateState($scope.item);
+    $scope.$watch("item", function() {
+        updateState($scope.item);
+    }, true);
 
 }]);
 
